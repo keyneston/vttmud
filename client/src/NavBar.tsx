223ee4dd5be8fc0a705f7dea522d2d292ef725c8
Tabitha <tabitha@keyneston.com>
2023-04-06 13:13:25 +0200

    More tweaking of CSS alignment

--- client/src/NavBar.tsx
+++ client/src/NavBar.tsx
@@ -1,10 +1,17 @@
 import "./NavBar.css";
+import { Avatar } from "primereact/avatar";
 
 function NavBar() {
 	return (
 		<div id="navbar">
 			<div id="navbar-left">VTTMUD</div>
-			<div id="navbar-right">TODO</div>
+			<div id="navbar-right">
+				<Avatar
+					label="U"
+					size="xlarge"
+					style={{ backgroundColor: "#2196F3", color: "#ffffff" }}
+				/>
+			</div>
 		</div>
 	);
 }
