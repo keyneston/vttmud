d002963e2cf4e295609d937987c77973817b4896
Tabitha <tabitha@keyneston.com>
2023-04-07 19:19:55 +0200

    Prettier actually ran

--- client/src/CraftTemplate.tsx
+++ client/src/CraftTemplate.tsx
@@ -7,28 +7,35 @@ import { AutoComplete } from "primereact/autocomplete";
 import { Panel } from "primereact/panel";
 import { Divider } from "primereact/divider";
 import { MinItemLevel, MaxItemLevel } from "./constants";
-import { Item, Gold } from './api/items';
+import { Item, Gold } from "./api/items";
 
-var itemsDB: Item[] = []
+var itemsDB: Item[] = [];
 
-getItemsDB()
+getItemsDB();
 
 function CraftTemplate() {
 	const [name, setName] = useState<string>(() => {
 		const character_name = localStorage.getItem("character_name");
-		return character_name || ""
+		return character_name || "";
 	});
-	const [item, setItem] = useState<Item|void>();
+	const [item, setItem] = useState<Item | void>();
 	const [itemCount, setItemCount] = useState<number>(1);
 	const [level, setLevel] = useState<number>(0);
 	const [days, setDays] = useState<number>(4);
 	const [endDate, setEndDate] = useState<Date>(new Date());
 
-	localStorage.setItem("character_name", name)
+	localStorage.setItem("character_name", name);
 
 	return (
 		<Panel header="Crafting Template">
-			<Output name={name} item={item} days={days} endDate={endDate} level={level} itemCount={itemCount}/>
+			<Output
+				name={name}
+				item={item}
+				days={days}
+				endDate={endDate}
+				level={level}
+				itemCount={itemCount}
+			/>
 
 			<Divider />
 
@@ -43,7 +50,7 @@ function CraftTemplate() {
 						<label htmlFor="character">Character Name</label>
 					</span>
 				</div>
-				<ItemAutoComplete item={item} setLevel={setLevel} setItem={setItem}/>
+				<ItemAutoComplete item={item} setLevel={setLevel} setItem={setItem} />
 				<span className="p-float-label">
 					<InputNumber
 						value={level}
@@ -57,16 +64,16 @@ function CraftTemplate() {
 
 				<div>
 					<span className="p-float-label">
-					<Calendar
-						value={endDate}
-						onChange={(e) => {
-							let d = Array.isArray(e.value) ? e.value[0] : e.value;
-							setEndDate(new Date(d || new Date()));
-						}}
-						id="endDate"
-					/>
-					<label htmlFor="endDate">End Date</label>
-				</span>
+						<Calendar
+							value={endDate}
+							onChange={(e) => {
+								let d = Array.isArray(e.value) ? e.value[0] : e.value;
+								setEndDate(new Date(d || new Date()));
+							}}
+							id="endDate"
+						/>
+						<label htmlFor="endDate">End Date</label>
+					</span>
 				</div>
 				<span className="p-float-label">
 					<InputNumber
@@ -78,136 +85,165 @@ function CraftTemplate() {
 					/>
 					<label htmlFor="days">Number of Days</label>
 				</span>
-				<ItemCount itemCount={itemCount} item={item} setItemCount={setItemCount}/>
+				<ItemCount itemCount={itemCount} item={item} setItemCount={setItemCount} />
 			</div>
 
 			<Divider />
-			<ItemInformation item={item} level={level||0} itemCount={itemCount} />
+			<ItemInformation item={item} level={level || 0} itemCount={itemCount} />
 		</Panel>
 	);
 }
 
-
-function Output({name, item, days, endDate, level, itemCount}: {name: string; item: void|Item|undefined; days: number; endDate: Date; level: number; itemCount: number;}) {
-		return (
-			<p className="m-0">
-				<b>Character: </b> {name} <br></br>
-				<b>Activity: </b>  Craft {itemCount > 1 ? ` ${itemCount} x ` : ""} {item ? item.name : ""}<br></br>
-				<b>Days: </b> {formatDate(subDate(endDate, days))}-{formatDate(endDate)}
-				<br></br>
-				<b>Item Level:</b> {level || 0} <b>DC:</b> {craftDC(level)}
-				<br></br>
-				<b>Result: Success Assurance</b>
-				<br></br>
-				<br></br>
-			</p>
-		)
+function Output({
+	name,
+	item,
+	days,
+	endDate,
+	level,
+	itemCount,
+}: {
+	name: string;
+	item: void | Item | undefined;
+	days: number;
+	endDate: Date;
+	level: number;
+	itemCount: number;
+}) {
+	return (
+		<p className="m-0">
+			<b>Character: </b> {name} <br></br>
+			<b>Activity: </b> Craft {itemCount > 1 ? ` ${itemCount} x ` : ""} {item ? item.name : ""}
+			<br></br>
+			<b>Days: </b> {formatDate(subDate(endDate, days))}-{formatDate(endDate)}
+			<br></br>
+			<b>Item Level:</b> {level || 0} <b>DC:</b> {craftDC(level)}
+			<br></br>
+			<b>Result: Success Assurance</b>
+			<br></br>
+			<br></br>
+		</p>
+	);
 }
 
-function ItemCount({itemCount, setItemCount, item}: {itemCount: number, setItemCount: (itemCount: number) => void, item: Item|void|undefined}) {
-		var consumable = false
-		item?.traits?.value.forEach((t: string) => {
-				if (t === "consumable") {
-						consumable = true
-				}
-		})
-
-		if (consumable === false) {
-				setItemCount(1)
+function ItemCount({
+	itemCount,
+	setItemCount,
+	item,
+}: {
+	itemCount: number;
+	setItemCount: (itemCount: number) => void;
+	item: Item | void | undefined;
+}) {
+	var consumable = false;
+	item?.traits?.value.forEach((t: string) => {
+		if (t === "consumable") {
+			consumable = true;
 		}
+	});
 
-		return (
-				<span className="p-float-label">
-						<InputNumber
-								value={itemCount}
-								onChange={(e) => setItemCount(e.value || 1)}
-								id="itemCount"
-								min={1}
-								max={4}
-								disabled={!consumable}
-						/>
-						<label htmlFor="itemCount">Item Count</label>
-				</span>
-		)
+	if (consumable === false) {
+		setItemCount(1);
+	}
+
+	return (
+		<span className="p-float-label">
+			<InputNumber
+				value={itemCount}
+				onChange={(e) => setItemCount(e.value || 1)}
+				id="itemCount"
+				min={1}
+				max={4}
+				disabled={!consumable}
+			/>
+			<label htmlFor="itemCount">Item Count</label>
+		</span>
+	);
 }
 
 type ItemAutoCompleteProps = {
-		item: void|Item|undefined;
-		setLevel: (level: number)=>void;
-		setItem: (item: void|Item|undefined)=> void;
-}
+	item: void | Item | undefined;
+	setLevel: (level: number) => void;
+	setItem: (item: void | Item | undefined) => void;
+};
 
-function ItemAutoComplete({item, setLevel, setItem}: ItemAutoCompleteProps) {
-		const [searchEntries, setSearchEntires] = useState<Item[]|void>()
+function ItemAutoComplete({ item, setLevel, setItem }: ItemAutoCompleteProps) {
+	const [searchEntries, setSearchEntires] = useState<Item[] | void>();
 
-		return (
-				<span className="p-float-label">
-						<AutoComplete
-								id="item"
-								value={item}
-								suggestions={searchEntries || undefined}
-								completeMethod={(e: any) => {
-										var term = e.query.toLowerCase()
-										var results = itemsDB.filter((entry) => entry.name.toLowerCase().includes(term))
-										setSearchEntires(results)
-								}}
-								field="name"
-								onChange={(e) => {
-										setItem(e.value)
-								}}
-								onSelect={(e) => {
-										setLevel(e.value.level)
-								}}
-								itemTemplate={(e) => {
-										return `${e.name} [${e.level}]`
-								}}
-								forceSelection />
-						<label htmlFor="item">Item</label>
-				</span>
-		)
+	return (
+		<span className="p-float-label">
+			<AutoComplete
+				id="item"
+				value={item}
+				suggestions={searchEntries || undefined}
+				completeMethod={(e: any) => {
+					var term = e.query.toLowerCase();
+					var results = itemsDB.filter((entry) =>
+						entry.name.toLowerCase().includes(term)
+					);
+					setSearchEntires(results);
+				}}
+				field="name"
+				onChange={(e) => {
+					setItem(e.value);
+				}}
+				onSelect={(e) => {
+					setLevel(e.value.level);
+				}}
+				itemTemplate={(e) => {
+					return `${e.name} [${e.level}]`;
+				}}
+				forceSelection
+			/>
+			<label htmlFor="item">Item</label>
+		</span>
+	);
 }
 
 type ItemInformationProps = {
-		item: void|Item|undefined;
-		level: number;
-		itemCount: number;
-}
+	item: void | Item | undefined;
+	level: number;
+	itemCount: number;
+};
 
-function ItemInformation({item, level, itemCount}: ItemInformationProps) {
-	var itemCost = item ? simplifyGold(item.cost) : 0
-	var formula = formulaCost(level)
-
+function ItemInformation({ item, level, itemCount }: ItemInformationProps) {
+	var itemCost = item ? simplifyGold(item.cost) : 0;
+	var formula = formulaCost(level);
 
 	return (
 		<div className="box">
-			<div className="template-label"> <label>Item Cost</label>
-			</div>
-			<div className="template-value">{itemCount > 1 ? `${itemCount} x ` : ""} {itemCost} gp</div>
 			<div className="template-label">
-					<label>Formula Cost</label>
+				{" "}
+				<label>Item Cost</label>
+			</div>
+			<div className="template-value">
+				{itemCount > 1 ? `${itemCount} x ` : ""} {itemCost} gp
+			</div>
+			<div className="template-label">
+				<label>Formula Cost</label>
 			</div>
 			<div className="template-value">{formula} gp</div>
 			<div className="template-label">
-					<label>Total Cost</label>
+				<label>Total Cost</label>
 			</div>
-			<div className="template-value">{itemCost*itemCount} + {formula} = {itemCost*itemCount + formulaCost(level)} gp</div>
+			<div className="template-value">
+				{itemCost * itemCount} + {formula} = {itemCost * itemCount + formulaCost(level)} gp
+			</div>
 		</div>
-	)
+	);
 }
 
 function getItemsDB() {
-	console.log("itemsDB: Starting fetch")
-	return fetch('/items.db.json')
-   .then((response) => response.json())
-   .then((responseJson) => {
-	 console.log("itemsDB: Updated")
-     itemsDB = responseJson
-   })
-   .catch((error) => {
-     console.error(error);
-   });
+	console.log("itemsDB: Starting fetch");
+	return fetch("/items.db.json")
+		.then((response) => response.json())
+		.then((responseJson) => {
+			console.log("itemsDB: Updated");
+			itemsDB = responseJson;
+		})
+		.catch((error) => {
+			console.error(error);
+		});
 }
-
 
 function subDate(date: Date, days: number): Date {
 	if (isNaN(days)) {
@@ -247,27 +283,27 @@ function craftDC(level: number) {
 }
 
 function simplifyGold(input: Gold): number {
-		var value = 0
-		console.log(`simplifyGold(${JSON.stringify(input)})`)
+	var value = 0;
+	console.log(`simplifyGold(${JSON.stringify(input)})`);
 
-		if (!input) {
-				return value
-		}
+	if (!input) {
+		return value;
+	}
 
-		if (input.pp) {
-				value += input.pp* 10
-		}
-		if (input.gp) {
-				value += input.gp
-		}
-		if (input.sp) {
-				value += input.sp/ 10
-		}
-		if (input.cp) {
-				value += input.cp/ 100
-		}
+	if (input.pp) {
+		value += input.pp * 10;
+	}
+	if (input.gp) {
+		value += input.gp;
+	}
+	if (input.sp) {
+		value += input.sp / 10;
+	}
+	if (input.cp) {
+		value += input.cp / 100;
+	}
 
-		return value
+	return value;
 }
 
 export { CraftTemplate };
