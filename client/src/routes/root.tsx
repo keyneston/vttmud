f63eaf2b464c3b839029a5542e01b63cad5a5797
Tabitha <tabitha@keyneston.com>
2023-04-15 23:20:55 +0200

    Redirect to proper domain

--- client/src/routes/root.tsx
+++ client/src/routes/root.tsx
@@ -4,7 +4,24 @@ import { CookiesProvider } from "react-cookie";
 import { Outlet } from "react-router-dom";
 import "./root.scss";
 
+const checkDomain = () => {
+	const domain = process.env.REACT_APP_DOMAIN;
+	if (!domain) {
+		return;
+	}
+
+	var proto = window.location.protocol;
+	var hostname = window.location.host;
+	var urlWithoutPort = `${proto}//${hostname}`;
+
+	if (domain !== urlWithoutPort) {
+		window.location.replace(window.location.href.replace(urlWithoutPort, domain));
+	}
+};
+
 export default function Root() {
+	checkDomain();
+
 	return (
 		<>
 			<div className="root">
