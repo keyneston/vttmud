de8e987756a2af3399607251421cce71674438a0
Tabitha <tabitha@keyneston.com>
2023-04-17 21:17:15 +0200

    Add editing on character logs

--- client/src/routes/log.tsx
+++ client/src/routes/log.tsx
@@ -2,20 +2,25 @@ import { useFormik, FormikValues, FormikErrors } from "formik";
 import { useParams, useNavigate } from "react-router-dom";
 import { useQuery, useQueryClient } from "@tanstack/react-query";
 import { useState, useMemo } from "react";
-import { DataTable } from "primereact/datatable";
 import { ReactNode } from "react";
-import { Column } from "primereact/column";
-import { Button } from "primereact/button";
-import { Dialog } from "primereact/dialog";
-import { InputText } from "primereact/inputtext";
 import { money2string, Gold } from "../api/items";
 import { updateLog, getLog, CharacterLogEntry } from "../api/characters";
 import { GoldEntry } from "../components/GoldEntry";
 import { ExperienceEntry } from "../components/ExperienceEntry";
+import { changeLogEntry } from "../api/characters";
+
+import { DataTable, DataTableRowEditCompleteEvent } from "primereact/datatable";
+import { Column } from "primereact/column";
+import { Button } from "primereact/button";
+import { Dialog } from "primereact/dialog";
+import { InputText } from "primereact/inputtext";
+import { InputNumber } from "primereact/inputnumber";
+
 import "./log.scss";
 
 export default function CharacterLog() {
 	const urlParams = useParams();
+	const queryClient = useQueryClient();
 	const [visible, setVisible] = useState(false);
 
 	const id = parseInt(urlParams.id || "0");
@@ -48,32 +53,24 @@ export default function CharacterLog() {
 		return `${createdAt.getFullYear()}-${createdAt.getMonth() + 1}-${createdAt.getDate()}`;
 	};
 
-	/*
-	const updateLogEntry = (row: number, entry: CharacterLogEntry) => {
-		let _logEntries = [...logEntries];
-
-		_logEntries[row] = entry;
-		setLogEntries(_logEntries);
-	};
-
 	const onRowEditComplete = async (e: DataTableRowEditCompleteEvent) => {
-		const res = await changeLogEntry({
+		await changeLogEntry({
 			id: e.newData.id,
 			characterID: e.newData.characterID,
 			spend: e.newData.spend,
 			description: e.newData.description,
 			gold: e.newData.gold,
+			experience: e.newData.experience,
 		});
 
-		updateLogEntry(e.index, res);
+		queryClient.invalidateQueries(["character", id]);
 	};
 
 	const goldEditor = (options: any) => {
-		console.log(options);
 		return (
 			<InputNumber
-				value={options.rowData.gold}
-				onValueChange={(e) => options.editorCallback(e.value)}
+				value={options.value}
+				onValueChange={(e: any) => options.editorCallback(e.value)}
 				mode="currency"
 				currency="EUR"
 				locale="en-US"
@@ -81,17 +78,19 @@ export default function CharacterLog() {
 		);
 	};
 
-	const descriptionEditor = (options: any) => {
-		console.log("descriptionOptions: ", options);
+	const textEditor = (options: any) => {
 		return (
 			<InputText
 				type="text"
 				value={options.value}
-				onChange={(e: any) => options.editorCallback(e.value)}
+				onChange={(e: any) => options.editorCallback(e.target.value)}
 			/>
 		);
 	};
-*/
+
+	const experienceEditor = (options: any) => {
+		return <InputNumber value={options.value} onChange={(e: any) => options.editorCallback(e.value)} />;
+	};
 
 	return (
 		<>
@@ -133,15 +132,34 @@ export default function CharacterLog() {
 					paginator
 					rows={20}
 					rowsPerPageOptions={[20, 50, 100]}
+					editMode="row"
+					dataKey="id"
+					onRowEditComplete={onRowEditComplete}
 				>
 					<Column field="date" header="Date" body={formatDate} />
-					<Column field="money" header="Money" body={(e) => money2string(e.gold)} />
 					<Column
-						field="exp"
+						field="gold"
+						header="Money"
+						body={(e) => money2string(e.gold)}
+						editor={goldEditor}
+					/>
+					<Column
+						field="experience"
 						header="Experience"
 						body={(e) => (e.experience !== 0 ? e.experience : "")}
+						editor={experienceEditor}
 					/>
-					<Column field="desc" header="Description" body={(e) => e.description} />
+					<Column
+						field="description"
+						header="Description"
+						body={(e) => e.description}
+						editor={textEditor}
+					/>
+					<Column
+						rowEditor
+						headerStyle={{ width: "10%", minWidth: "8rem" }}
+						bodyStyle={{ textAlign: "center" }}
+					></Column>
 				</DataTable>
 			</div>
 		</>
