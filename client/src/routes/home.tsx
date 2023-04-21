b2c819cb528ca5dd5f08e8f3ef3b6bbef24409da
Tabitha <tabitha@keyneston.com>
2023-04-21 14:14:58 +0200

    Revamp home pge

--- client/src/routes/home.tsx
+++ client/src/routes/home.tsx
@@ -1,53 +1,97 @@
+import dayjs from "dayjs";
+import advancedFormat from "dayjs/plugin/advancedFormat";
+import { Panel } from "primereact/panel";
+import { Fragment, ReactNode } from "react";
+import "./Home.scss";
+
+dayjs.extend(advancedFormat);
+
+type Entry = {
+	date: dayjs.Dayjs;
+	tasks: string[];
+};
+
+const logEntries: Entry[] = [
+	{
+		date: dayjs("2023-04-20"),
+		tasks: [
+			"Add avatar to log pages to indicate which character is selected",
+			"Allow setting custom items when crafting",
+		],
+	},
+	{
+		date: dayjs("2023-04-19"),
+		tasks: ["Added cropping tool to crop avatar images before uploading"],
+	},
+	{
+		date: dayjs("2023-04-18"),
+		tasks: ["Add editing of downtime entries", "Display remaining downtime on downtime log"],
+	},
+	{
+		date: dayjs("2023-04-17"),
+		tasks: [
+			"Fix sorting of downtime entries",
+			"Default level/craft dcs in downtime entries to current level",
+			"Add editing of existing character log entries",
+			"Fix bug where downtime log would record into the future",
+			"Added bonus field to bulk set bonus on downtime logs",
+		],
+	},
+	{
+		date: dayjs("2023-04-16"),
+		tasks: [
+			"Added log for tracking uses of downtime, UI is rudimentary at this point",
+			"Added pie chart for Activity type in Downtime Log",
+		],
+	},
+	{
+		date: dayjs("2023-04-15"),
+		tasks: [
+			"Added Homepage with changelog",
+			"Link on Character Sheet to download JSON",
+			"Navigation Sidebar",
+		],
+	},
+];
+
 export default function Home() {
 	return (
-		<>
-			<h2>Welcome</h2>
-			<p>
-				Welcome to VTTMud. This site is a work in progress. Please contact{" "}
-				<a href="https://discordapp.com/users/620653174469951512">
-					<i className="pi pi-discord" /> @keyneston#2930
-				</a>{" "}
-				on discord if you have feedback.
-			</p>
+		<div className="home-root">
+			<Panel header="Welcome">
+				<p>
+					Welcome to VTTMud. This site is a work in progress. Please contact{" "}
+					<a href="https://discordapp.com/users/620653174469951512">
+						<i className="pi pi-discord" /> @keyneston#2930
+					</a>{" "}
+					on discord if you have feedback.
+				</p>
+			</Panel>
+			<Changelog data={logEntries} />
+		</div>
+	);
+}
 
-			<h2>Changelog</h2>
+function Changelog({ data }: { data: Entry[] }) {
+	return (
+		<Panel header="Changelog">
 			<ul>
-				<li>April 20th</li>
-				<ul>
-					<li>Add avatar to log pages to indicate which character is selected</li>
-					<li>Allow setting custom items when crafting</li>
-				</ul>
-				<li>April 19th</li>
-				<ul>
-					<li>Added cropping tool to crop avatar images before uploading</li>
-				</ul>
-				<li>April 18th</li>
-				<ul>
-					<li>Add editing of downtime entries</li>
-					<li>Display remaining downtime on downtime log</li>
-				</ul>
-				<li>April 17th</li>
-				<ul>
-					<li>Fix sorting of downtime entries</li>
-					<li>Default level/craft dcs in downtime entries to current level</li>
-					<li>Add editing of existing character log entries</li>
-					<li>Fix bug where downtime log would record into the future</li>
-					<li>Added bonus field to bulk set bonus on downtime logs</li>
-				</ul>
-				<li>April 16th</li>
-				<ul>
-					<li>
-						Added log for tracking uses of downtime, UI is rudimentary at this point
-					</li>
-					<li>Added pie chart for Activity type in Downtime Log</li>
-				</ul>
-				<li>April 15th</li>
-				<ul>
-					<li>Added Homepage with changelog</li>
-					<li>Link on Character Sheet to download JSON</li>
-					<li>Navigation Sidebar</li>
-				</ul>
+				{data.map((e: any): ReactNode => {
+					return <FormatEntry entry={e} />;
+				})}
 			</ul>
-		</>
+		</Panel>
+	);
+}
+
+function FormatEntry({ entry }: { entry: Entry }) {
+	return (
+		<Fragment key={`changelog-${entry.date.format("yyyy-mm-DD")}`}>
+			<li>{entry.date.format("MMM Do")}</li>
+			<ul>
+				{entry.tasks.map((t) => {
+					return <li>{t}</li>;
+				})}
+			</ul>
+		</Fragment>
 	);
 }
